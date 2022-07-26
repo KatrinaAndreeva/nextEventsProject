b9ede54b4d4a340cb8bdebb6c54323fc42a454c8
Kate <katya-andreeva@ukt.net>
2022-07-26 18:24:50 +0300

    "added get/post options"

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../helpers/api-util';
 import Head from 'next/head';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
 function HomePage(props) {
   return (
@@ -9,6 +10,7 @@ function HomePage(props) {
         <title>NextJS Events</title>
         <meta name="description" content="Find a lot of great events" />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
